993471a094520e207e1a5984df8e27a3669f567f
Montival Junior <montival_junior@yahoo.com.br>
2020-05-24 15:13:12 -0300

    Adicionado annotation @EnableJpaAuditing na classe que efetua o startup do Spring Boot

--- src/main/java/com/challenge/service/classes/UserService.java
+++ src/main/java/com/challenge/service/classes/UserService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-import static java.time.LocalDateTime.now;
-
 @Service
 @AllArgsConstructor
 public class UserService implements UserServiceInterface {
